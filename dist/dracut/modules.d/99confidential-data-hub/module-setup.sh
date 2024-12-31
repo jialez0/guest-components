7b60e10c20698093dc4cfb3a462b510df4e6483a
Xynnn007 <xynnn@linux.alibaba.com>
2024-12-31 11:50:03 +0800

    dracut: add dracut module for AA and CDH
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

---
+++ dist/dracut/modules.d/99confidential-data-hub/module-setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+check() {
+    return 0
+}
+
+depends() {
+    return 0
+}
+
+install() {
+    inst_multiple /usr/local/bin/confidential-data-hub
+    inst_simple $moddir/confidential-data-hub.toml /etc/trustiflux/confidential-data-hub.toml
+}
